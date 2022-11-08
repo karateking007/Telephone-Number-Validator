0cfdb2b8919cd9e108199b193917e81a502c56f5
Riley <rileylitchfield95@gmail.com>
2022-11-07 22:36:56 -0600

    Updated to use arrow function and ternary operator

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-function telephoneCheck(str) {
-    const regex = /^(1?)+(\s?)?((\((\d){3}\))|((\d){3}))((-|\s)?)(\d){3}(-|\s)?((\d){4})$/;
+const regex = /^(1?)+(\s?)?((\((\d){3}\))|((\d){3}))((-|\s)?)(\d){3}(-|\s)?((\d){4})$/;
 
-    if (regex.test(str)) {
-        return true;
-    } else {
-        return false;
-    }
+let telephoneCheck = (str) => {
+    return regex.test(str) ? true : false
 }
 
 telephoneCheck("5555555555");
