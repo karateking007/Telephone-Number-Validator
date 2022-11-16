054acac33b6977af9838b280a052a4a53e74c07e
Riley <rileylitchfield95@gmail.com>
2022-11-15 19:17:33 -0600

    Cleaned up code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const regex = /^(1?)+(\s?)?((\((\d){3}\))|((\d){3}))((-|\s)?)(\d){3}(-|\s)?((\d){4})$/;
 
 let telephoneCheck = (str) => {
-    return regex.test(str) ? true : false
+    return regex.test(str)
 }
 
 telephoneCheck("5555555555");
